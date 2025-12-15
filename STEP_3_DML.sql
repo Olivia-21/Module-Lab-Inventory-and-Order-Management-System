1c6d647292767b47eedc73be62fdef186417dc23
Olivia Dosimey <olivia.dosimey@amalitech.com>
2025-12-15 16:32:45 +0000

    Code refactored

--- STEP_3_DML.sql
+++ STEP_3_DML.sql
@@ -1,27 +1,27 @@
 -- Business KPI Queries
 -- Total Revenue (Shipped or Delivered)
-SELECT SUM(TotalAmount) AS TotalRevenue
+SELECT SUM(total_order_amount) AS total_revenue
 FROM Orders
-WHERE OrderStatus IN ('Shipped', 'Delivered');
+WHERE order_status IN ('Shipped', 'Delivered');
 
 -- Top 10 Customers by Spending
-SELECT c.FullName, SUM(o.TotalAmount) AS TotalSpent
+SELECT c.full_name, SUM(o.total_order_amount) AS total_spent
 FROM Customers c
-JOIN Orders o ON c.CustomerID = o.CustomerID
-GROUP BY c.CustomerID, c.FullName
-ORDER BY TotalSpent DESC
+JOIN Orders o ON c.customer_id = o.customer_id
+GROUP BY c.customer_id, c.full_name
+ORDER BY total_spent DESC
 LIMIT 10;
 
 -- Top 5 Best-Selling Products by Quantity
-SELECT p.ProductName, SUM(oi.Quantity) AS TotalSold
+SELECT p.product_name, SUM(oi.quantity) AS total_sold
 FROM Products p
-JOIN OrderItems oi ON p.ProductID = oi.ProductID
-GROUP BY p.ProductID, p.ProductName
-ORDER BY TotalSold DESC
+JOIN OrderItems oi ON p.product_id = oi.product_id
+GROUP BY p.product_id, p.product_name
+ORDER BY total_sold DESC
 LIMIT 5;
 
 -- Monthly Sales Trend
-SELECT DATE_FORMAT(OrderDate, '%Y-%m') AS Month, SUM(TotalAmount) AS TotalRevenue
+SELECT DATE_FORMAT(order_date, '%Y-%m') AS Month, SUM(total_order_amount) AS TotalRevenue
 FROM Orders
 GROUP BY Month
 ORDER BY Month;
@@ -30,42 +30,43 @@ ORDER BY Month;
 -- Analytical Queries using Window Functions
 -- Sales Rank by Category
 SELECT
-    p.Category,
-    p.ProductName,
-    SUM(oi.Quantity * oi.PriceAtPurchase) AS Revenue,
-    RANK() OVER (PARTITION BY p.Category ORDER BY SUM(oi.Quantity * oi.PriceAtPurchase) DESC) AS RankInCategory
+    p.category,
+    p.product_name,
+    SUM(oi.quantity * oi.price_at_purchase) AS Revenue,
+    RANK() OVER (PARTITION BY p.category ORDER BY SUM(oi.quantity * oi.price_at_purchase) DESC) AS rank_in_category
 FROM Products p
-JOIN OrderItems oi ON p.ProductID = oi.ProductID
-GROUP BY p.Category, p.ProductName;
+JOIN OrderItems oi ON p.product_id = oi.product_id
+GROUP BY p.Category, p.product_name;
 
 -- Customer Order Frequency
 SELECT
-    c.FullName,
-    o.OrderID,
-    o.OrderDate,
-    LAG(o.OrderDate) OVER (PARTITION BY c.CustomerID ORDER BY o.OrderDate) AS PreviousOrderDate
+    c.full_name,
+    o.order_id,
+    o.order_date,
+    LAG(o.order_date) OVER (PARTITION BY c.customer_id ORDER BY o.order_date) AS previous_order_date
 FROM Customers c
-JOIN Orders o ON c.CustomerID = o.CustomerID
-ORDER BY c.CustomerID, o.OrderDate;
+JOIN Orders o ON c.customer_id = o.customer_id
+ORDER BY c.customer_id, o.order_date;
 
 
 -- Creating a View for Customer Sales Summary
 CREATE VIEW CustomerSalesSummary AS
 SELECT 
-    c.CustomerID,
-    c.FullName,
-    SUM(o.TotalAmount) AS TotalSpent
-FROM Customers c
-JOIN Orders o ON c.CustomerID = o.CustomerID
-GROUP BY c.CustomerID, c.FullName;
+    c.customer_id,
+    c.full_name,
+    SUM(o.total_order_amount) AS total_spent
+FROM Customers cexit
+
+JOIN Orders o ON c.customer_id = o.customer_id
+GROUP BY c.customer_id, c.full_name;
 
 
 DELIMITER //
 
 CREATE PROCEDURE ProcessNewOrder(
-    IN p_CustomerID INT,
-    IN p_ProductID INT,
-    IN p_Quantity INT
+    IN p_customer_id INT,
+    IN p_product_id INT,
+    IN p_quantity INT
 )
 BEGIN
     DECLARE v_Price DECIMAL(10,2);
@@ -75,10 +76,10 @@ BEGIN
     START TRANSACTION;
 
     -- Check inventory
-    SELECT Price, QuantityOnHand INTO v_Price, v_Stock
+    SELECT Price, quantity_on_hand INTO v_Price, v_Stock
     FROM Products p
-    JOIN Inventory i ON p.ProductID = i.ProductID
-    WHERE p.ProductID = p_ProductID;
+    JOIN Inventory i ON p.product_id = i.product_id
+    WHERE p.product_id = p_product_id;
 
     IF v_Stock < p_Quantity THEN
         ROLLBACK;
@@ -86,18 +87,18 @@ BEGIN
     ELSE
         -- Reduce inventory
         UPDATE Inventory
-        SET QuantityOnHand = QuantityOnHand - p_Quantity
-        WHERE ProductID = p_ProductID;
+        SET quantity_on_hand = quantity_on_hand - p_quantity
+        WHERE product_id = p_product_id;
 
         -- Create new order
-        INSERT INTO Orders (CustomerID, OrderDate, TotalAmount, OrderStatus)
-        VALUES (p_CustomerID, CURDATE(), v_Price * p_Quantity, 'Pending');
+        INSERT INTO Orders (customer_id, order_date, total_order_amount, order_status)
+        VALUES (p_customer_id, CURDATE(), v_Price * p_quantity, 'Pending');
 
         SET v_OrderID = LAST_INSERT_ID();
 
         -- Insert into order items
-        INSERT INTO OrderItems (OrderID, ProductID, Quantity, PriceAtPurchase)
-        VALUES (v_OrderID, p_ProductID, p_Quantity, v_Price);
+        INSERT INTO OrderItems (order_id, product_id, Quantity, price_at_purchase)
+        VALUES (v_OrderID, p_product_id, p_quantity, v_Price);
 
         COMMIT;
     END IF;
--- Step_2_DDL.sql
+++ Step_2_DDL.sql
@@ -1,95 +1,134 @@
 -- STEP 2: SQL DDL CREATE TABLES
-Create Database e_commercedb;
+Create Database IF NOT EXISTS e_commercedb;
 Use e_commercedb;
+# drop Database e_commercedb;
 
 -- 1. Customers Table
 CREATE TABLE Customers (
-    CustomerID INT PRIMARY KEY AUTO_INCREMENT,
-    FullName VARCHAR(100) NOT NULL,
-    Email VARCHAR(100),
-    Phone VARCHAR(20),
-    ShippingAddress VARCHAR(200)
+
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100),
+    phone VARCHAR(20),
+    shipping_address VARCHAR(200)
 );
+
+
+
 
 -- 2. Products Table
 CREATE TABLE Products (
-    ProductID INT PRIMARY KEY AUTO_INCREMENT,
-    ProductName VARCHAR(100) NOT NULL,
-    Category VARCHAR(50),
-    Price DECIMAL(10,2) NOT NULL CHECK (Price >= 0)
+    product_id INT PRIMARY KEY AUTO_INCREMENT,
+    product_name VARCHAR(100) NOT NULL,
+    category VARCHAR(50),
+    price DECIMAL(10,2) NOT NULL CHECK (price >= 0)
 );
 
 -- 3. Inventory Table
 CREATE TABLE Inventory (
-    ProductID INT PRIMARY KEY,
-    QuantityOnHand INT NOT NULL CHECK (QuantityOnHand >= 0),
-    FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+    product_id INT PRIMARY KEY,
+    quantity_on_hand INT NOT NULL CHECK (quantity_on_hand >= 0),
+    FOREIGN KEY (product_id) REFERENCES Products(product_id)
 );
 
-
+DROP TABLE Orders;
 -- 4. Orders Table
 CREATE TABLE Orders (
-    OrderID INT PRIMARY KEY AUTO_INCREMENT,
-    CustomerID INT NOT NULL,
-    OrderDate DATE NOT NULL,
-    TotalAmount DECIMAL(10,2) NOT NULL CHECK (TotalAmount >= 0),
-    OrderStatus VARCHAR(20) NOT NULL,
-    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT NOT NULL,
+    order_date DATE NOT NULL,
+    total_order_amount DECIMAL(10,2) NOT NULL CHECK (total_order_amount >= 0),
+    order_status VARCHAR(20) NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 -- 5. OrderItems Table
 CREATE TABLE OrderItems (
-    OrderItemID INT PRIMARY KEY AUTO_INCREMENT,
-    OrderID INT NOT NULL,
-    ProductID INT NOT NULL,
-    Quantity INT NOT NULL CHECK (Quantity > 0),
-    PriceAtPurchase DECIMAL(10,2) NOT NULL CHECK (PriceAtPurchase >= 0),
-    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
-    FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+    order_item_id INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL CHECK (quantity > 0),
+    price_at_purchase DECIMAL(10,2) NOT NULL CHECK (price_at_purchase >= 0),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES Products(product_id)
 );
 
 
 
 -- Insert Sample Customers
-INSERT INTO Customers (FullName, Email, Phone, ShippingAddress)
+INSERT INTO Customers (full_name, email, phone, shipping_address)
 VALUES 
-('Olivia Dosimey', 'olivia.dosimey@amalitech.com', '0509903401', 'WS-545-6642'),
+('Olivia Dosimey', 'olivia.dosimey@amalitech.com', '050-990-3401', 'WS-545-6642'),
 ('Evans', 'evans@gmail.com', '245-889-8901', '456 Adagya St'),
-('Michael Jackson', 'michael@gmail.com', '345-678-9012', 'WT Pine Rd');
+('Michael Jackson', 'michael@gmail.com', '345-678-9012', 'WT Pine Rd'),
+('Henry Nana Antwi', 'nanaantwi@gmail.com', '035-119-5567', 'ER Kofo Rd'),
+('Michael Jackson', 'michael@gmail.com', '248-111-2212', 'BA Bio Rd'),
+('Henry Morgan', 'hmorgan@gmail.com', '123-517-3212', 'Ohio Rd'),
+('Keziah Fordjour', 'keziah@gmail.com', '657-914-4912', 'AS Edom Rd'),
+('Lily Amoh', 'lily@gmail.com', '248-111-2212', 'WS Benmack Rd'),
+('Leonard Messi', 'leonard@gmail.com', '248-111-2212', 'WT Ohio Rd'),
+('Emmanuel Godson', 'godson@gmail.com', '543-876-2109', 'AS Adum Rd');
 
 
 -- Insert Sample Products
-INSERT INTO Products (ProductName, Category, Price)
+INSERT INTO Products (product_name, category, price)
 VALUES
 ('Laptop', 'Electronics', 1200.00),
 ('T-Shirt', 'Apparel', 25.00),
 ('Headset', 'Electronics', 40.00),
+('Rich Dad Poor Dad', 'Books', 70.00),
+('Wrist Watch', 'Apparel', 30.00),
+('Iphone', 'Electronics', 2500.00),
+('Laptop Bag', 'Apparel', 170.00),
+('Wireless Mouse', 'Electronics', 50.00),
 ('Hard drive', 'Electronics', 150.00),
 ('Jeans', 'Apparel', 60.00);
 
-
 -- Insert Inventory Data
-INSERT INTO Inventory (ProductID, QuantityOnHand)
+INSERT INTO Inventory (product_id, quantity_on_hand)
 VALUES
 (1, 10),
 (2, 50),
 (3, 30),
 (4, 20),
-(5, 40);
+(5, 40),
+(6, 60),
+(7, 70),
+(8, 15),
+(9, 55),
+(10, 85);
 
 -- Insert Sample Orders
-INSERT INTO Orders (CustomerID, OrderDate, TotalAmount, OrderStatus)
+INSERT INTO Orders (customer_id, order_date, total_order_amount, order_status)
 VALUES
 (1, '2025-12-01', 1225.00, 'Shipped'),
-(2, '2025-12-02', 85.00, 'Delivered'),
-(1, '2025-12-05', 150.00, 'Pending');
+(2, '2025-12-02', 85.00, 'Pending'),
+(3, '2025-12-04', 85.00, 'Pending'),
+(4, '2025-12-06', 85.00, 'Delivered'),
+(5, '2025-12-07', 85.00, 'Shipped'),
+(6, '2025-12-09', 85.00, 'Delivered'),
+(7, '2025-12-09', 85.00, 'Shipped'),
+(8, '2025-12-12', 85.00, 'Delivered'),
+(9, '2025-12-05', 85.00, 'Pending'),
+(10, '2025-12-06', 150.00, 'Delivered');
 
 -- Insert OrderItems
-INSERT INTO OrderItems (OrderID, ProductID, Quantity, PriceAtPurchase)
+INSERT INTO OrderItems (order_id, product_id, quantity, price_at_purchase)
 VALUES
 (1, 1, 1, 1200.00),
-(1, 2, 1, 25.00),
-(2, 2, 2, 25.00),
-(2, 5, 1, 35.00),
-(3, 4, 1, 150.00);
+(1, 2, 2, 50.00),
+(2, 2, 3, 75.00),
+(2, 5, 5, 155.00),
+(2, 5, 7, 220.00),
+(2, 5, 9, 700.00),
+(2, 5, 4, 400.00),
+(2, 5, 6, 35.00),
+(2, 5, 8, 45.00),
+(3, 4, 9, 150.00);
+
+SELECT * FROM Customers;
+SELECT * FROM Products;
+SELECT * FROM Inventory;
+SELECT * FROM Orders;
+SELECT * FROM OrderItems;
 
